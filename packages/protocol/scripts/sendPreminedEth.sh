1c0993bed182d60ed99cda9fb5258bd7cd87de0f
Keszey Dániel <keszeyd@MacBook-Pro.local>
2025-02-06 14:40:08 +0100

    drain eth for faucet

---
+++ packages/protocol/scripts/sendPreminedEth.sh
@@ -0,0 +1,3 @@
+forge script scripts/sendToDispenser.s.sol --rpc-url http://localhost:32002 -vvvv --broadcast --legacy
+forge script scripts/sendToDispenser.s.sol --rpc-url http://localhost:32005 -vvvv --broadcast --legacy
+forge script scripts/sendToDispenser.s.sol --rpc-url http://localhost:32006 -vvvv --broadcast --legacy
