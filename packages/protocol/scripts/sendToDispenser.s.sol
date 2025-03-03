a67260639638be671fd9046426c15f54e5f9b8f6
Keszey Dániel <keszeyd@MacBook-Pro.local>
2025-03-03 12:09:31 +0100

    finishing touches before devnet

--- packages/protocol/scripts/sendToDispenser.s.sol
+++ packages/protocol/scripts/sendToDispenser.s.sol
@@ -10,9 +10,9 @@ contract sendToDispenser is Script {
     
     // Array of private keys for all pre-mined accounts
     uint256[] private accounts = [
-        0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31,
-        0x39725efee3fb28614de3bacaffe4cc4bd8c436257e2c8bb887c4b5c4be45e76d,
-        0x53321db7c1e331d93a11a41d16f004d7ff63972ec8ec7c25db329728ceeb1710,
+        //0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31,
+        //0x39725efee3fb28614de3bacaffe4cc4bd8c436257e2c8bb887c4b5c4be45e76d,
+        //0x53321db7c1e331d93a11a41d16f004d7ff63972ec8ec7c25db329728ceeb1710,
         0xab63b23eb7941c1251757e24b3d2350d2bc05c3c388d06f8fe6feafefb1e8c70,
         0x5d2344259f42259f82d2c140aa66102ba89b57b4883ee441a8b312622bd42491,
         0x27515f805127bebad2fb9b183508bdacb8c763da16f54e0678b16e8f28ef3fff,
